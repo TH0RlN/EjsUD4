8a9b89f98754f18d0c0e3694ed9d062146de3402
Raul Arias Perez <aprariasperez@gmail.com>
2022-11-11 12:48:46 +0100

    asdasd

--- persona.js
+++ persona.js
@@ -18,11 +18,11 @@
 
 function main()
 {
-    /*var Persona = function (nombre, edad, genero)
+    function Persona()
     {
-        this.nombre =   nombre;
-        this.edad   =   edad;
-        this.genero =   genero;
+        this.nombre =   "";
+        this.edad   =   "";
+        this.genero =   "";
 
         this.obtDetalles = function ()
         {
@@ -34,72 +34,36 @@ function main()
         };
     }
 
-    var Alumno = function (nombre, edad, genero, curso, grupo)
+    Alumno.prototype = new Persona();
+    function Alumno()
     {
-        Persona.call(this, nombre, edad, genero);
-
-        this.curso = curso;
-        this.grupo = grupo;
+        this.curso = "";
+        this.grupo = "";
 
         this.registrar = function ()
         {
         };
     }
 
-    var Profesor = function (nombre, edad, genero, asignatura, nivel)
-    {
-        Persona.call(this, nombre, edad, genero);
-
-        this.asignatura = asignatura;
-        this.nivel = nivel;
+    function Profesor()
+    {;
+        this.asignatura = "";
+        this.nivel = "";
 
         this.asignar = function ()
         {
         };
     }
 
-    var persona = new Profesor("Juan", 30, "hombre", "1ero", "B");
+    /*var alumno      = new Alumno("Juan", 30, "hombre", "1ero", "B");
+    var profesor    = new Profesor("Juan", 30, "hombre", "Matematicas", "Alto");
 
-    document.getElementById("body").innerHTML += persona.obtDetalles();*/
+    document.getElementById("body").innerHTML += alumno.obtDetalles();
+    document.getElementById("body").innerHTML += "<br>";
+    document.getElementById("body").innerHTML += profesor.obtDetalles();*/
 
-    var persona =
-    {
-        nombre      :   "Juan",
-        edad        :   30,
-        genero      :   "Masculino",
+    console.log(Object.getPrototypeOf(new Persona("Juan", 16, "Masculino")));
 
-        obtDetalles :   function ()
-        {
-            var str = "";
-            for (const key in this)
-                if (!(typeof(this[key]) == 'function' || typeof(this[key]) == 'object'))
-                    str += key + " : " + this[key] + "<br>";
-            return str;
-        }
-    }
-
-    var alumno =
-    {
-        curso       :   "1ero",
-        grupo       :   "B",
-
-        registrar   :   function ()
-        {}
-    }
-    
-    var profesor =
-    {
-        asignatura  :   "Matematicas",
-        nivel       :   "Avanzado",
-
-        asignar     :   function ()
-        {}
-    }
-
-    alumno.prototype    = persona.constructor;
-    profesor.prototype  = persona.constructor;
-
-    document.getElementById("body").innerHTML += profesor.obtDetalles();
 }
 
 main();
